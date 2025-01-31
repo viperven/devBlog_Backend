5b9684dfaa0f7615751736ce507f1b37d521c0e7
Rupesh Jha <rj187225@gmail.com>
2025-01-31 16:28:47 +0530

    feat : setup db config env and index.js

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-console.log("har har mahadev");
+
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const http = require("http"); // For creating the HTTP server
+const app = express();
+
+
+const connectDB = require("./src/config/db");
+const server = http.createServer(app);
+
+app.use(cors(
+    {
+        origin: [
+            "http://localhost:3000",
+            "http://localhost:5173",
+            "https://mydevtinder.netlify.app",
+        ],
+        methods: ["GET", "POST"],
+    }
+))
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: ["http://localhost:5173", "https://mydevtinder.netlify.app"],
+        methods: ["GET", "POST", "PUT", "PATCH"],
+        credentials: true,
+    })
+);
+
+//all Routes
+
+// app.use("/auth", require("./src/routes/authRoutes"));
+// app.use("/profile", require("./src/routes/profileRoutes"));
+// app.use("/request", require("./src/routes/connectionRoutes"));
+// app.use("/user", require("./src/routes/userRotutes"));
+// app.use("/message", require("./src/routes/messageRoute"));
+
+//first connect to db then start listening to api calls
+connectDB()
+    .then(() => {
+        console.log("Database connection established");
+        server.listen(5000, () => {
+            console.log("server working at 5000");
+        });
+    })
+    .catch((err) => {
+        console.log("database connection error", err);
+    });
+
+
+app.get("/", async (req, res) => {
+    res.status(200).send("ok");
+});
+
---
+++ src/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+const connectDB = async () => {
+  await mongoose.connect(process.env.devConnection);
+};
+
+module.exports = connectDB;
