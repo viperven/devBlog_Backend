e5b52c2c96d60773257832b23032ab743bed1341
Rupesh Jha <rj187225@gmail.com>
2025-01-31 16:29:10 +0530

    feat : added auth ,acivity,post model

---
+++ src/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+const validator = require("validator")
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email address");
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true
+    },
+    profilePicture: {
+        type: String,
+        trim: true,
+        validate(value) {
+            if (value && !validator.isURL(value)) {
+                throw new Error("Invalid URL");
+            }
+        }
+    },
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
